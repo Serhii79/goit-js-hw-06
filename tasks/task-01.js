9f55ff9cd02f7e761c7e73df47879d98b565befd
Serhii Bondar <sergiibondar1979@gmail.com>
2020-10-30 14:57:19 +0200

    Create task-01

---
+++ tasks/task-01.js
@@ -0,0 +1,16 @@
+// Write code under this line
+
+
+const getUserNames = (users) => users.map(({ name }) => name);
+
+console.log(getUserNames(users));
+
+/* [
+  "Moore Hensley",
+  "Sharlene Bush",
+  "Ross Vazquez",
+  "Elma Head",
+  "Carey Barr",
+  "Blackburn Dotson",
+  "Sheree Anthony",
+] */
